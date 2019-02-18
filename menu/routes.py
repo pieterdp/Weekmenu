2b434dcefbdd1d32805f82671d138cce328792f3
Pieter De Praetere <pieter.de.praetere@helptux.be>
2019-02-18 17:55:45 +0100

    Some more mockupping

--- menu/routes.py
+++ menu/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
+from datetime import datetime
 
 
 gui = Blueprint('gui', __name__)
@@ -13,4 +14,6 @@ def index():
 @gui.route('/week', methods=['GET'])
 @gui.route('/week/<string:week>', methods=['GET'])
 def week_overview(week=None):
+    if not week:
+        week = datetime.now().strftime('%U')
     return render_template('weekmenu/week.html', week=week)
